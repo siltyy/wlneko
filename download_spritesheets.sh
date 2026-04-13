156ecfd892c7df1efce9cd70fca62c7b32517f14
Emma Tebibyte <emma@tebibyte.media>
2026-04-13 07:29:37 +0000

    make download_spritesheets.sh more portable

--- download_spritesheets.sh
+++ download_spritesheets.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-mkdir -p assets
-curl -s https://bomvel.neocities.org/neko/ \
+if command -v curl >/dev/null 2>&1; then
+	alias fetch='curl -s'
+	alias dl='curl -O'
+else 
+	alias fetch='ftp -o-'
+	alias dl='ftp'
+fi
+
+CWD="$PWD"
+mkdir -p assets && cd assets
+fetch https://bomvel.neocities.org/neko/ \
   | grep -E 'src="./sheets/.*"' \
   | sed -E 's#.*src="./sheets/([[:print:]]+\.png)".*#https://bomvel.neocities.org/neko/sheets/\1#g' \
-  | while read -r i ;do curl -O "$i" --output-dir assets & done
+  | while read -r i ;do dl "$i" & done
+
+cd "$CWD
