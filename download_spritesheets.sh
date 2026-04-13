820afa26e4352d81d5d8fc69d13aafb6b6f9e016
silt <silt@0xC3.win>
2026-04-13 07:10:01 +0000

    whyyy does posix still lack lazy quantifiers

--- download_spritesheets.sh
+++ download_spritesheets.sh
@@ -2,6 +2,6 @@
 
 mkdir -p assets
 curl -s https://bomvel.neocities.org/neko/ \
-  | grep -E 'src="./sheets/.*?"' \
-  | sed -E 's#.*src="./sheets/([[:print:]]+?\.png)".*#https://bomvel.neocities.org/neko/sheets/\1#g' \
+  | grep -E 'src="./sheets/.*"' \
+  | sed -E 's#.*src="./sheets/([[:print:]]+\.png)".*#https://bomvel.neocities.org/neko/sheets/\1#g' \
   | while read -r i ;do curl -O "$i" --output-dir assets & done
